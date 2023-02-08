82edc4a73cf80b79f472d4f1e59683813c8091dd
pastfuture-music <124755572+pastfuture-music@users.noreply.github.com>
2023-02-08 17:33:23 +0800

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ class MyDocument extends Document {
           <meta name="description" content="Listen and download sheets composed or arranged by PastFuture." />
           <meta name="keywords" content="PastFuture, pastfuture, kyle, music, compositions, ykh"/>
           <meta name="copyright" content="Copyright © 2022 PastFuture Music & Seaweed Melodies. All rights reserved." />
-          <meta name="author" content="YKH" />
+          <meta name="author" content="Yu-Kai, Huang" />
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://www.pastfuture.tk" />
           <meta property="og:title" content="PastFuture's Music Site" />
@@ -20,7 +20,7 @@ class MyDocument extends Document {
           <meta property="twitter:title" content="PastFuture's Music Site" />
           <meta property="twitter:description" content="Listen and download sheets composed or arranged by PastFuture." />
           <meta property="twitter:image" content="https://musescore.com/static/musescore/userdata/cover/f/7/6/28704246.jpg" />
-          <link rel="icon" href="https://raw.githubusercontent.com/pastfuture777/music-sheets/main/public/favicon.ico" />
+          <link rel="icon" href="https://raw.githubusercontent.com/pastfuture-music/hostdrive/main/public/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           {siteConfig.googleFontLinks.map(link => (
